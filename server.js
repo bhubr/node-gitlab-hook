0daecdf7da1cc571b140904c0db446f4a4e7f672
Benoît Hubert <benoithubert@gmail.com>
2017-07-03 11:43:22 +0200

    git pull output log, dont return bits if no matches

--- server.js
+++ server.js
@@ -15,14 +15,15 @@ config.logger = {
 const listener = webhooks(config, genericCallback);
 
 function extractGitPullOutput(gitPullOutput) {
+  console.log('git pull output', gitPullOutput);
   const re = /^From (https:\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-]))?\s+((\[new branch\]|[0-9a-f]+\.\.[0-9a-f]+)\s+([0-9a-zA-Z\_]+)\s+\->\s([0-9a-zA-Z\_]+)\/([0-9a-zA-Z\_]+))$/gm;
   const matches = re.exec(gitPullOutput);
-  return {
+  return matches ? {
     repoUrl: matches[1],
     localBranch: matches[6],
     remoteName: matches[7],
     remoteBranch: matches[8]
-  };
+  } : matches;
 }
 
 /**
