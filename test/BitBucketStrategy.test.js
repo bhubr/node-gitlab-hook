d2908e8c7d235722f5e510f1b8306a3a03bd8755
Benoît Hubert <benoithubert@gmail.com>
2017-07-03 15:13:07 +0200

    fix extract issue:edited for BitBucket

--- test/BitBucketStrategy.test.js
+++ test/BitBucketStrategy.test.js
@@ -26,22 +26,22 @@ describe('BitBucketStrategy tests', () => {
 
   it('extract "issue edited" event', done => {
     const headers = {
-      'x-github-event': 'issues'
+      'x-event-key': 'issue:updated'
     };
-    const strategy = new GitHubStrategy(headers);
-    const payload = tools.getSamplePayload('github', 'issue-edited');
+    const strategy = new BitBucketStrategy(headers);
+    const payload = tools.getSamplePayload('bitbucket', 'issue-edited');
     strategy.setData(payload);
     const eventData = strategy.getEventData();
-    console.log(eventData);
-    const repoUrl = 'https://github.com/bhubr/test-webhook';
+    const repoUrl = 'https://bitbucket.org/bhubr/test-webhook';
     const { event, data } = eventData;
     const { issue, repository } = data;
+    const issueHtmlUrl = 'https://bitbucket.org/bhubr/test-webhook/issues/1/sample-issue-edited';
     assert.equal(event, 'issue:edited', "Event should be 'issue:edited'");
     assert.equal(issue.title, 'sample issue edited', "Issue title should be 'sample issue edited'");
     assert.equal(issue.body, 'lorem ipsum dolor sucks', "Issue body should be ''lorem ipsum dolor sucks'");
     assert.equal(issue.number, 1, "Issue number should be 1");
     assert.equal(issue.state, 'open', "Issue state should be 'open'");
-    assert.equal(issue.htmlUrl, 'https://github.com/bhubr/test-webhook/issues/1', "Issue url should be 'https://github.com/bhubr/test-webhook/issues/1'");
+    assert.equal(issue.htmlUrl, issueHtmlUrl, "Issue url should be '" + issueHtmlUrl + "'");
     assert.equal(repository.name, 'test-webhook', "Repo name should be 'test-webhook'");
     assert.equal(repository.fullName, 'bhubr/test-webhook', "Repo name should be 'bhubr/test-webhook'");
     assert.equal(repository.url, repoUrl, "Repo url should be '" + repoUrl + "'");
