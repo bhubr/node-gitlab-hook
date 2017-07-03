c954fbe019433f8d0618a3a266117949cb631c1b
Benoît Hubert <benoithubert@gmail.com>
2017-07-03 12:08:33 +0200

    fix regex

--- randomstuff/gitPull.js
+++ randomstuff/gitPull.js
@@ -1,5 +1,5 @@
 function extractGitPullOutput(gitPullOutput) {
-  const re = /^From (https:\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-]))?\s+((\[new branch\]|[0-9a-f]+\.\.[0-9a-f]+)\s+([0-9a-zA-Z\_]+)\s+\->\s([0-9a-zA-Z\_]+)\/([0-9a-zA-Z\_]+))$/gm;
+  const re = /^From (https:\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-]))?\n\s+((\[new branch\]|[0-9a-f]+\.\.[0-9a-f]+)\s+([0-9a-zA-Z\_]+)\s+\->\s([0-9a-zA-Z\_]+)\/([0-9a-zA-Z\_]+))$/gm;
   const matches = re.exec(gitPullOutput);
   return matches ? {
     repoUrl: matches[1],
@@ -11,7 +11,7 @@ function extractGitPullOutput(gitPullOutput) {
 
 // var gitPullOutput = 'From https://github.com/bhubr/test-webhook\n ' +
 //    '9a76574..6be2c7c  master     -> origin/master';
-var gitPullOutput = ' From https://github.com/bhubr/node-github-gitlab-bitbucket-hook\n' +
+var gitPullOutput = 'From https://github.com/bhubr/node-github-gitlab-bitbucket-hook\n' +
    '40a5043..151d7ac  6-git-pull-output -> origin/6-git-pull-output';
 
 console.log('git pull output\n', gitPullOutput, extractGitPullOutput(gitPullOutput));
--- server.js
+++ server.js
@@ -16,13 +16,14 @@ const listener = webhooks(config, genericCallback);
 
 function extractGitPullOutput(gitPullOutput) {
   console.log('git pull output', gitPullOutput);
-  const re = /^From (https:\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-]))?\s+((\[new branch\]|[0-9a-f]+\.\.[0-9a-f]+)\s+([0-9a-zA-Z\_]+)\s+\->\s([0-9a-zA-Z\_]+)\/([0-9a-zA-Z\_]+))$/gm;
+  const re = /^From (https:\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-]))?\n\s*(\[new branch\]|[0-9a-f]+\.\.[0-9a-f]+)\s+([0-9a-zA-Z\-\_]+)\s+\->\s([0-9a-zA-Z\_\-]+)\/([0-9a-zA-Z\_\-]+)$/gm;
   const matches = re.exec(gitPullOutput);
+  console.log(matches);
   return matches ? {
     repoUrl: matches[1],
-    localBranch: matches[6],
-    remoteName: matches[7],
-    remoteBranch: matches[8]
+    localBranch: matches[5],
+    remoteName: matches[6],
+    remoteBranch: matches[7]
   } : matches;
 }
 
