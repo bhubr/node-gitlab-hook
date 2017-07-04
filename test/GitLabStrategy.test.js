b29726378d76a7141b1093076f31121f727c4df4
Benoît Hubert <benoithubert@gmail.com>
2017-07-04 07:19:13 +0200

    Handle pullrequest:created for GitLab

--- test/GitLabStrategy.test.js
+++ test/GitLabStrategy.test.js
@@ -53,6 +53,7 @@ describe('GitLabStrategy tests', () => {
     assert.equal(repository.url, repoUrl, "Repo url should be '" + repoUrl + "'");
     done();
   });
+
   it('extract "issue edited" event', done => {
     const headers = {
       'x-gitlab-event': 'Issue Hook'
@@ -71,6 +72,39 @@ describe('GitLabStrategy tests', () => {
     assert.equal(issue.number, 27, "Issue number should be 27");
     assert.equal(issue.state, 'open', "Issue state should be 'open'");
     assert.equal(issue.htmlUrl, issueUrl, "Issue url should be '" + issueUrl + "'");
+    assert.equal(repository.name, 'foobar', "Repo name should be 'foobar'");
+    assert.equal(repository.fullName, 'goodkarma/foobar', "Repo name should be 'goodkarma/foobar'");
+    assert.equal(repository.url, repoUrl, "Repo url should be '" + repoUrl + "'");
+    done();
+  });
+
+  it('extract "pull request created" event', done => {
+    const headers = {
+      'x-gitlab-event': 'Merge Request Hook'
+    };
+    const strategy = new GitLabStrategy(headers);
+    const payload = tools.getSamplePayload('gitlab', 'pullrequest-created');
+    strategy.setData(payload);
+    const eventData = strategy.getEventData();
+    const repoUrl = 'https://gitlab.com/goodkarma/foobar';
+    const pullUrl = 'https://gitlab.com/goodkarma/foobar/merge_requests/6';
+    const pullBody = 'Hotfix for #32\r\nYay :)';
+    const { event, data } = eventData;
+    const { pullRequest, repository } = data;
+    assert.equal(event, 'pullrequest:created', "Event should be 'pullrequest:created'");
+    assert.equal(pullRequest.title, 'Source branch', "Pull request title should be 'Source branch'");
+    assert.equal(pullRequest.body, pullBody, "Pull request body should be '" + pullBody + "'");
+    assert.equal(pullRequest.number, 6, "Pull request number should be 6");
+    assert.equal(pullRequest.state, 'open', "Pull request state should be 'open'");
+    assert.equal(pullRequest.htmlUrl, pullUrl, "Pull request url should be '" + pullUrl + "'");
+    assert.equal(pullRequest.sourceBranch, 'source-branch', "Pull request's source branch should be 'source-branch'");
+    assert.equal(pullRequest.targetBranch, 'master', "Pull request's target branch should be 'master'");
+    assert.equal(pullRequest.sourceRepo.name, 'foobar', "Repo name should be 'foobar'");
+    assert.equal(pullRequest.sourceRepo.fullName, 'goodkarma/foobar', "Repo name should be 'goodkarma/foobar'");
+    assert.equal(pullRequest.sourceRepo.url, repoUrl, "Repo url should be '" + repoUrl + "'");
+    assert.equal(pullRequest.targetRepo.name, 'foobar', "Repo name should be 'foobar'");
+    assert.equal(pullRequest.targetRepo.fullName, 'goodkarma/foobar', "Repo name should be 'goodkarma/foobar'");
+    assert.equal(pullRequest.targetRepo.url, repoUrl, "Repo url should be '" + repoUrl + "'");
     assert.equal(repository.name, 'foobar', "Repo name should be 'foobar'");
     assert.equal(repository.fullName, 'goodkarma/foobar', "Repo name should be 'goodkarma/foobar'");
     assert.equal(repository.url, repoUrl, "Repo url should be '" + repoUrl + "'");
