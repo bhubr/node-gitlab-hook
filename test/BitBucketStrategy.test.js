023d6e798ba0f9fe0f4ff79f170c34af9f85ba1b
Benoît Hubert <benoithubert@gmail.com>
2017-07-04 06:08:44 +0200

    extract pullrequest:created done for GitHub&BitBucket

--- test/BitBucketStrategy.test.js
+++ test/BitBucketStrategy.test.js
@@ -71,4 +71,38 @@ describe('BitBucketStrategy tests', () => {
     assert.equal(repository.url, repoUrl, "Repo url should be '" + repoUrl + "'");
     done();
   });
+
+  it('extract "pull request created" event', done => {
+    const headers = {
+      'x-event-key': 'pullrequest:created'
+    };
+    const strategy = new BitBucketStrategy(headers);
+    const payload = tools.getSamplePayload('bitbucket', 'pullrequest-created');
+    strategy.setData(payload);
+    const eventData = strategy.getEventData();
+    const repoUrl = 'https://bitbucket.org/bhubr/test-webhook';
+    const pullUrl = 'https://bitbucket.org/bhubr/test-webhook/pull-requests/1';
+    const pullBody = '* commit stuff 1\n\n* commit stuff 2\n\n* commit stuff 3';
+    const { event, data } = eventData;
+    const { pullRequest, repository } = data;
+    assert.equal(event, 'pullrequest:created', "Event should be 'pullrequest:created'");
+    assert.equal(pullRequest.title, 'Source branch', "Pull request title should be 'Source branch'");
+    assert.equal(pullRequest.body, pullBody, "Pull request body should be '" + pullBody + "'");
+    assert.equal(pullRequest.number, 1, "Pull request number should be 1");
+    assert.equal(pullRequest.state, 'open', "Pull request state should be 'open'");
+    assert.equal(pullRequest.htmlUrl, pullUrl, "Pull request url should be '" + pullUrl + "'");
+    assert.equal(pullRequest.sourceBranch, 'source-branch', "Pull request's source branch should be 'source-branch'");
+    assert.equal(pullRequest.targetBranch, 'master', "Pull request's target branch should be 'master'");
+    assert.equal(pullRequest.sourceRepo.name, 'test-webhook', "Repo name should be 'test-webhook'");
+    assert.equal(pullRequest.sourceRepo.fullName, 'bhubr/test-webhook', "Repo name should be 'bhubr/test-webhook'");
+    assert.equal(pullRequest.sourceRepo.url, repoUrl, "Repo url should be '" + repoUrl + "'");
+    assert.equal(pullRequest.targetRepo.name, 'test-webhook', "Repo name should be 'test-webhook'");
+    assert.equal(pullRequest.targetRepo.fullName, 'bhubr/test-webhook', "Repo name should be 'bhubr/test-webhook'");
+    assert.equal(pullRequest.targetRepo.url, repoUrl, "Repo url should be '" + repoUrl + "'");
+    assert.equal(repository.name, 'test-webhook', "Repo name should be 'test-webhook'");
+    assert.equal(repository.fullName, 'bhubr/test-webhook', "Repo name should be 'bhubr/test-webhook'");
+    assert.equal(repository.url, repoUrl, "Repo url should be '" + repoUrl + "'");
+    done();
+  });
+
 });
--- test/GitHubStrategy.test.js
+++ test/GitHubStrategy.test.js
@@ -80,4 +80,37 @@ describe('GitHubStrategy tests', () => {
     assert.equal(repository.url, repoUrl, "Repo url should be '" + repoUrl + "'");
     done();
   });
+
+  it('extract "pull request created" event', done => {
+    const headers = {
+      'x-github-event': 'pull_request'
+    };
+    const strategy = new GitHubStrategy(headers);
+    const payload = tools.getSamplePayload('github', 'pullrequest-created');
+    strategy.setData(payload);
+    const eventData = strategy.getEventData();
+    const repoUrl = 'https://github.com/bhubr/test-webhook';
+    const pullUrl = 'https://github.com/bhubr/test-webhook/pull/3';
+    const pullBody = 'Merge source branche => master.\nDo stuff.\nEnjoy.';
+    const { event, data } = eventData;
+    const { pullRequest, repository } = data;
+    assert.equal(event, 'pullrequest:created', "Event should be 'pullrequest:created'");
+    assert.equal(pullRequest.title, 'Source branch', "Pull request title should be 'Source branch'");
+    assert.equal(pullRequest.body, pullBody, "Pull request body should be '" + pullBody + "'");
+    assert.equal(pullRequest.number, 3, "Pull request number should be 3");
+    assert.equal(pullRequest.state, 'open', "Pull request state should be 'open'");
+    assert.equal(pullRequest.htmlUrl, pullUrl, "Pull request url should be '" + pullUrl + "'");
+    assert.equal(pullRequest.sourceBranch, 'source-branch', "Pull request's source branch should be 'source-branch'");
+    assert.equal(pullRequest.targetBranch, 'master', "Pull request's target branch should be 'master'");
+    assert.equal(pullRequest.sourceRepo.name, 'test-webhook', "Repo name should be 'test-webhook'");
+    assert.equal(pullRequest.sourceRepo.fullName, 'bhubr/test-webhook', "Repo name should be 'bhubr/test-webhook'");
+    assert.equal(pullRequest.sourceRepo.url, repoUrl, "Repo url should be '" + repoUrl + "'");
+    assert.equal(pullRequest.targetRepo.name, 'test-webhook', "Repo name should be 'test-webhook'");
+    assert.equal(pullRequest.targetRepo.fullName, 'bhubr/test-webhook', "Repo name should be 'bhubr/test-webhook'");
+    assert.equal(pullRequest.targetRepo.url, repoUrl, "Repo url should be '" + repoUrl + "'");
+    assert.equal(repository.name, 'test-webhook', "Repo name should be 'test-webhook'");
+    assert.equal(repository.fullName, 'bhubr/test-webhook', "Repo name should be 'bhubr/test-webhook'");
+    assert.equal(repository.url, repoUrl, "Repo url should be '" + repoUrl + "'");
+    done();
+  });
 });
